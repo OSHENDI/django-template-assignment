73c0dd6f370e5970f6c78139d09e243e517e09c4
Osos <oalhendi03@gmail.com>
2025-12-11 17:19:28 +0200

    added filter and formatted code

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+import datetime
 
 
 def index(request):
     profile = {
-        'full_name': 'Osama Alhendi',
-        'student_id': '120211752',
-        'email': 'os@iugaza.edu.ps',
-        'major': 'IT - Software Development',
-        'description': 'I am an IT student at IUG with an interest in web development. Currently learning about the Django framework and how to use it to create web applications.',
-        'course': 'Advanced Web Programming',
+        "first_name": "osama",
+        "last_name": "alhendi",
+        "student_id": "120211752",
+        "address": "gaza - deir albalah",
+        "email": "",
+        "major": "IT - software development",
+        "dob": datetime.date(2003, 1, 1),
+        "description": "I am an IT student at IUG with an interest in web development. Currently learning about the Django framework and how to use it to create web applications.",
+        "course": "Advanced Web Programming",
     }
-    return render(request, 'pages/index.html', context=profile)
+    return render(request, "pages/index.html", context=profile)
